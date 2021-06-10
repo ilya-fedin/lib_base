bb029ad2b3643b85aaa4bc178293a2c268188a5b
Ilya Fedin <fedin-ilja2010@ya.ru>
2021-06-10 05:23:19 +0400

    Provide patched copies of Wayland shell integration for client-side shadow

---
+++ base/platform/linux/base_linux_xdg_shell_integration.cpp
@@ -0,0 +1,11 @@
+// This file is part of Desktop App Toolkit,
+// a set of libraries for developing nice desktop applications.
+//
+// For license and copyright information please follow this link:
+// https://github.com/desktop-app/legal/blob/master/LEGAL
+//
+#include "base/platform/linux/base_linux_xdg_shell_integration.h"
+
+#include <QtCore/QtPlugin>
+
+Q_IMPORT_PLUGIN(DesktopAppWaylandXdgShellIntegrationPlugin)
